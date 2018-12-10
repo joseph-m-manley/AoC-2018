f7a3054fcd005424f090c85a2d258812e226295f
Joe.Manley <joe.manley@blackboard.com>
2018-12-10 15:49:43 -0500

    Finished Day 3: part 2

--- Day 3/part1.py
+++ Day 3/part1.py
@@ -25,6 +25,8 @@ class Claim():
         self.right = self.left + int(size[0])
         self.bottom = self.top + int(size[1])
 
+        self.did_overlap = False
+
     def __repr__(self):
         return self.claim
 
@@ -38,31 +40,35 @@ class Claim():
         if not self.overlaps(other):
             return set()
 
+        self.did_overlap = True
+        other.did_overlap = True
+
         left = max(self.left, other.left)
         right = min(self.right, other.right)
         top = max(self.top, other.top)
         bottom = min(self.bottom, other.bottom)
 
-        points = set()
-        for x in range(left, right):
-            for y in range(top, bottom):
-                points.add( (x, y) )
-
-        return points
+        return { (x, y) for y in range(top, bottom) for x in range(left, right) }
 
 
 def find_overlapping_claims():
     claims = get_claims()
     overlapping_points = set()
+    visited = list()
 
-    while len(claims) > 0:
+    while len(claims) > 1:
         claim = claims.pop()
+
         for other in claims:
             points = claim.try_get_overlapping_points(other)
             overlapping_points.update(points)
 
+        visited.append(claim)
+
     print('Total overlapping area: {0} sqin'.format(len(overlapping_points)))
 
+    no_overlap = list(filter(lambda x: not x.did_overlap, visited))
+    print(no_overlap)
 
 if __name__ == '__main__':
     find_overlapping_claims()
